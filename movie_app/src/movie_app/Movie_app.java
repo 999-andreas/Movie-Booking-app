8b904e0588ab2e711d272babe9e505204965789e
Andreas Chatel <andreas.chatel5@gmail.com>
2022-11-30 22:12:58 +0100

    debut du projet
    ajout de petits trucs nuls

---
+++ movie_app/src/movie_app/Movie_app.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package movie_app;
+
+/**
+ *
+ * @author andre
+ */
+public class Movie_app {
+    
+    private int onestla;
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ movie_app/src/movie_app/movie.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package movie_app;
+
+import java.util.Date;
+
+/**
+ *
+ * @author andre
+ */
+public class movie {
+    private int id;
+    private String genre;
+    private String title;
+    private Date date;
+    private int duration;
+    private double price;
+    private boolean availability;
+    private int nb_place;
+    private double discount;
+
+    public movie(int id, String genre, String title, Date date, int duration, double price, boolean availability, int nb_place, double discount) {
+        this.id = id;
+        this.genre = genre;
+        this.title = title;
+        this.date = date;
+        this.duration = duration;
+        this.price = price;
+        this.availability = availability;
+        this.nb_place = nb_place;
+        this.discount = discount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public int getNb_place() {
+        return nb_place;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+    
+    public void saveMovie() // bien mettre les test d'execption
+    {
+        //update le fichier avec une nouvelle ligne
+    }
+    
+    public movie getMovie(int id)
+    {
+        //vas chercher un filme en particulier en fonction de l'id (la ligne quoi)
+    }
+    
+    /*
+    problématique : 
+    comment avoir des id unique, sachant que on peut pas savoir le num de la dernière ligne 
+    
+    peut etre mettre des trucs aleatoire en fonction du temps
+    */
+}
+
