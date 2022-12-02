2977fd205de5db8a9c56ee5204cf110582b978c2
Antoine <antoinewellers@hotmail.fr>
2022-12-02 12:07:40 +0100

    Login Frame Creation
    Add a Login Frame, and a frame for the movies' list

--- movie_app/src/movie_app/Movie_app.java
+++ movie_app/src/movie_app/Movie_app.java
@@ -13,11 +13,6 @@ public class Movie_app {
     private int onestla;
     
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    
     
 }
