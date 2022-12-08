47995d330b44efbad08b6de6cefc8b9de4694c4d
Andreas Chatel <andreas.chatel5@gmail.com>
2022-12-08 18:06:11 +0100

    biling frame

--- movie_app/src/movie_app/MovieBilingFrame.java
+++ movie_app/src/movie_app/MovieBilingFrame.java
@@ -20,12 +20,20 @@ public class MovieBilingFrame extends javax.swing.JFrame {
     
     public MovieBilingFrame() {
         initComponents();
+        
     }
     
     public MovieBilingFrame(movie aMovie, Member aMember, int nb_tickets, double total) {
         initComponents();
         theMovie = aMovie;
         theMember = aMember;
+        
+        jLabel3.setText(theMember.getFirst_name());
+        jLabel4.setText(theMember.getLast_name());
+        jLabel11.setText(theMovie.getTitle());
+        jprice.setText(String.valueOf(theMovie.getPrice()));
+        jnb_tickets.setText(String.valueOf(theMovie.getNb_place()));
+        jLabel12.setText(String.valueOf(total)); 
     }
 
     /**
@@ -37,17 +45,109 @@ public class MovieBilingFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel2 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jprice = new javax.swing.JLabel();
+        jnb_tickets = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jDiscount = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+        jLabel12 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+
+        jLabel2.setText("jLabel2");
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("recipt:");
+
+        jLabel3.setText("first_name");
+
+        jLabel4.setText("last_name");
+
+        jLabel5.setText("title_movie:");
+
+        jprice.setText("price_ticket");
+
+        jnb_tickets.setText("nb_tickets");
+
+        jLabel8.setText("total_price: ");
+
+        jDiscount.setText("discount: ");
+
+        jLabel10.setText("x");
+
+        jLabel11.setText("jLabel11");
+
+        jLabel12.setText("jLabel12");
+
+        jLabel6.setText("jLabel6");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel8)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel12)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel5)
+                                .addGap(2, 2, 2)
+                                .addComponent(jLabel11)
+                                .addGap(57, 57, 57)
+                                .addComponent(jLabel1))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel3)
+                                .addGap(18, 18, 18)
+                                .addComponent(jLabel4))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jprice)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jLabel10)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jnb_tickets)
+                                .addGap(18, 18, 18)
+                                .addComponent(jDiscount)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jLabel6)))
+                        .addContainerGap(141, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(26, 26, 26)
+                .addComponent(jLabel1)
+                .addGap(36, 36, 36)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel4))
+                .addGap(34, 34, 34)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel11))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jprice)
+                    .addComponent(jLabel10)
+                    .addComponent(jnb_tickets)
+                    .addComponent(jDiscount)
+                    .addComponent(jLabel6))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel8)
+                    .addComponent(jLabel12))
+                .addContainerGap(338, Short.MAX_VALUE))
         );
 
         pack();
@@ -89,5 +189,18 @@ public class MovieBilingFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jDiscount;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jnb_tickets;
+    private javax.swing.JLabel jprice;
     // End of variables declaration//GEN-END:variables
 }
