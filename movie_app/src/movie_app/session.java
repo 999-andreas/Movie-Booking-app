bdc5d2c70bea1c47a61d5417e1b564825ae4256b
Andreas Chatel <andreas.chatel5@gmail.com>
2022-12-11 16:16:44 +0100

    adaptation de la class movie
    plus qu'à adapter tout ce qui repose sur la class movie (càd presque tout)

--- movie_app/src/movie_app/session.java
+++ movie_app/src/movie_app/session.java
@@ -9,8 +9,38 @@ package movie_app;
  * @author andre
  */
 public class session {
-    private String[] times;
+    private String time;
     private int nb_place;
     private double price;
+
+    public session(String time, int nb_place, double price) {
+        this.time = time;
+        this.nb_place = nb_place;
+        this.price = price;
+    }
+    
+    public String getTime() {
+        return time;
+    }
+
+    public int getNb_place() {
+        return nb_place;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setTime(String times) {
+        this.time = times;
+    }
+
+    public void setNb_place(int nb_place) {
+        this.nb_place = nb_place;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
     
 }
